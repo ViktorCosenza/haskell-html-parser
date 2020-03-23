27cb24a27ee090625ab709da348f1ed8611ff588
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2020-03-22 21:03:42 -0300

    Html inline attributes

--- Main.hs
+++ Main.hs
@@ -8,7 +8,7 @@ type HTMLAttributes = Map.Map String String
 
 data HTMLValue = HTMLText String
     | HTMLSeq [HTMLValue]
-    | HTMLField HTMLAttributes HTMLValue
+    | HTMLField String HTMLAttributes HTMLValue
     deriving (Show, Eq)
 
 type ParserInput = String
@@ -42,6 +42,24 @@ parseSpan :: (Char -> Bool) -> Parser String
 parseSpan f = Parser $ \input ->
     let (parsed, unparsed) = span f input in Right (ParserSuccess unparsed parsed)
 
+parseAny :: Parser String
+parseAny = parseSpan (/= '"')
+
+notEmpty :: (Eq (f a), Alternative f) => Parser (f a) -> Parser (f a) 
+notEmpty p = Parser $ \input -> do
+    (ParserSuccess unparsed parsed) <- runParser p input
+    if empty == parsed
+        then Left $ ParserError "Must not be empty" 
+        else Right $ ParserSuccess unparsed parsed 
+
+parseIn :: String -> Parser String
+parseIn = foldl1 (<|>) . parsers
+    where parsers = map (\c -> parseSpan (== c))
+   
+parseNotIn :: String -> Parser String
+parseNotIn = foldl1 (<|>) . parsers
+    where parsers = map (\c -> parseSpan (/= c))
+
 parseChar :: Char -> Parser Char
 parseChar x = Parser f 
     where 
@@ -62,49 +80,49 @@ parseString s = Parser $ \input ->
         Left (ParserError err) -> Left $ ParserError $ "Expected: " ++ s ++ " Found: " ++ err  
         result -> result
 
-parseAny :: Parser String
-parseAny = parseSpan (/= '"')
-
-parseNotIn :: String -> Parser String
-parseNotIn = foldl1 (<|>) . parsers
-    where parsers = map (\c -> parseSpan (/= c))
+parseName :: Parser String
+parseName = parseSpan isAlpha
 
 stringLiteral :: Parser String
 stringLiteral = parseChar '"' *> parseAny <* parseChar '"'
 
+
 htmlText :: Parser HTMLValue
 htmlText = HTMLText <$> stringLiteral
 
-htmlAttributes :: Parser HTMLAttributes
-htmlAttributes = Map.fromList <$> sepBy ws parseAttribute
+htmlAttributes :: Parser (String, HTMLAttributes)
+htmlAttributes = liftA2 (,) (parseName <* ws) (Map.fromList <$> sepBy ws parseAttribute)
     where
-        parseAttribute = liftA2 (,) (stringLiteral <* parseChar '=') stringLiteral
+        parseAttribute = liftA2 (,) (ws *> parseName <* ws <* parseChar '=') (ws *> stringLiteral)
 
 htmlField :: Parser HTMLValue
 htmlField = Parser (\input -> 
-        case runParser (parseChar '<' *> htmlAttributes <* parseChar '>') input of
+        case runParser (ws *> parseChar '<' *> ws *> ( htmlAttributes <* parseChar '>' )) input of
             (Left (ParserError e)) -> Left $ ParserError $ "AttributeParser: " ++ e
-            (Right (ParserSuccess unparsed as)) -> do
+            (Right (ParserSuccess unparsed (name, as))) -> do
                 (ParserSuccess unparsed' children) <- runParser htmlValue unparsed
-                Right $ ParserSuccess unparsed' $ HTMLField as children
+                Right $ ParserSuccess unparsed' $ HTMLField name as children
     )
 
-htmlValue :: Parser HTMLValue
-htmlValue = htmlSeq
 
-htmlSeq :: Parser HTMLValue
-htmlSeq = HTMLSeq <$> Parser f
+htmlValue :: Parser HTMLValue
+htmlValue = HTMLSeq <$> Parser f
     where 
         f input 
             | input == "" = Right $ ParserSuccess "" []
             | otherwise = do
-                (ParserSuccess unparsed parsed) <- runParser (many $ htmlField <|> htmlText) input
+                (ParserSuccess unparsed parsed) <- runParser ((many (ws *>htmlField <|> htmlText)) <* ws) input
                 case unparsed of 
                     "" -> Right $ ParserSuccess unparsed parsed 
-                    _ -> Left $ ParserError $ "Could not parse " ++ unparsed
+                    _ -> Left $ ParserError $ "Parsed: " ++ show parsed ++  " Unparsed: " ++ unparsed
 
 runHtmlParser :: String -> Either ParserError (ParserSuccess HTMLValue)
 runHtmlParser = runParser htmlValue
+
+parseFile :: FilePath -> IO ()
+parseFile f = do
+    content <- readFile f
+    print $ runHtmlParser content
 
 main :: IO ()
 main = do
